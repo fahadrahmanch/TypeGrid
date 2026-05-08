136053cde9492d0a5a06ba45d1c2ee353d03b6c9
fahad rahman ch <ff865880@gmail.com>
2026-05-09 02:30:00 +0530

    fix: update socket configuration

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,12 @@
+// import { io } from "socket.io-client";
+// const env = import.meta.env;
+// export const socket = io(env.VITE_API_URL, {
+//   transports: ["websocket"],
+//   withCredentials: true,
+// });
 import { io } from "socket.io-client";
-const env = import.meta.env;
-export const socket = io(env.VITE_API_URL, {
+
+export const socket = io("https://api.typegrid.in", {
   transports: ["websocket"],
   withCredentials: true,
 });
