def64a1e6a473d03a5e9a60684ae66fe6ad220e4
fahad rahman ch <ff865880@gmail.com>
2026-05-12 02:52:19 +0530

    refactor: update profile validation

--- backend/src/presentation/middlewares/validations/update-profile.validation.ts
+++ backend/src/presentation/middlewares/validations/update-profile.validation.ts
@@ -3,11 +3,35 @@ import joi from "joi";
 export const updateProfileValidation = joi.object({
   data: joi.object({
     name: joi.string().trim().min(3).max(50).optional(),
+
     email: joi.string().trim().email().optional(),
-    bio: joi.string().trim().min(10).max(200).allow("").optional(),
-    age: joi.alternatives().try(joi.number().min(9).max(100), joi.string().trim()).allow(null, "").optional(),
-    number: joi.alternatives().try(joi.string().trim().pattern(/^[6-9]\d{9}$/), joi.number()).allow("").optional(),
-    gender: joi.string().valid("Male", "Female", "Other", "", null).optional(),
-    imageUrl: joi.string().trim().uri().allow("").optional(),
+
+    bio: joi.string().trim().allow("").optional(),
+
+    age: joi
+      .alternatives()
+      .try(
+        joi.number().min(9).max(100),
+        joi.string().trim()
+      )
+      .allow("")
+      .optional(),
+
+    number: joi
+      .alternatives()
+      .try(
+        joi.string().trim().pattern(/^[6-9]\d{9}$/),
+        joi.number()
+      )
+      .allow("")
+      .optional(),
+
+    gender: joi
+      .string()
+      .valid("Male", "Female", "Other")
+      .allow("")
+      .optional(),
+
+    imageUrl: joi.string().trim().uri().optional(),
   }),
 });
